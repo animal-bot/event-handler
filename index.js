38c83cabbdc795dd7c49455de2be6bb8925bb379
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2020-12-12 17:03:21 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+// Rest of bot code
+
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
